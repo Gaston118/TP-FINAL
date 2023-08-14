0b07666e9bb91380bb0068fa6300d033575e41e4
mateoquaglia <117778104+mateoquaglia@users.noreply.github.com>
2023-08-14 20:42:12 -0300

    Update Politica.java

--- src/Recursos/Politica.java
+++ src/Recursos/Politica.java
@@ -8,13 +8,13 @@ public class Politica {
     }
 
     public Integer Politica_1(Integer[] t){
-        Integer minimo = Integer.MAX_VALUE;
+        Integer minimo = Integer.MAX_VALUE; //hacemos que el primer valor de rdp.getDisparos()[i] sea menor que minimo
         int posMin = 0;
 
-        for(int i=0; i<CANTIDAD_TRANSICIONES; i++){
-            if((rdp.getDisparos()[i]<minimo) && t[i]!=0){
-                minimo=rdp.getDisparos()[i];
-                posMin=i;
+        for(int i=0; i<CANTIDAD_TRANSICIONES; i++){ //recorro las transiciones 
+            if((rdp.getDisparos()[i]<minimo) && t[i]!=0){ //verifico si el valor del disparo es menor que "minimo"
+                minimo=rdp.getDisparos()[i];  //si se cumple actualizo el minimo 
+                posMin=i;                     //y actualizo la posicion del minimo 
             }
          }
         return posMin;
