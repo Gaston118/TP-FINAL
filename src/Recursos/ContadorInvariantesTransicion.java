b536bacd9dcbb7ed9c0ed14220adb5c39ee853e4
mateoquaglia <117778104+mateoquaglia@users.noreply.github.com>
2023-09-08 19:04:30 -0300

    Update ContadorInvariantesTransicion.java

--- src/Recursos/ContadorInvariantesTransicion.java
+++ src/Recursos/ContadorInvariantesTransicion.java
@@ -11,14 +11,15 @@ import java.util.regex.Pattern;
 public class ContadorInvariantesTransicion {
     public static void main(String[] args) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("transitions.txt"));
-            String line;
+            BufferedReader reader = new BufferedReader(new FileReader("transitions.txt")); //BufferedReader para leer el archivo de transiciones
+            String line;                                                                   //la uso para almacenar cada linea del archivo
 
-            Map<String, Integer> contadorInvariantes = new HashMap<>();
-
+            Map<String, Integer> contadorInvariantes = new HashMap<>();                     // mapa para almacenar el conteo de cada invariante
+            
+            // expresioon regular 
             Pattern patron = Pattern.compile("((T1 )(.*?)(T3 )(.*?)|(T2 )(.*?)(T4 )(.*?))((T5 )(.*?)(T7 )(.*?)|(T6 )(.*?)(T8 )(.*?))((T9 )(.*?)(T11 )(.*?)|(T10 )(.*?)(T12 )(.*?))(T13 )(.*?)(T14 )");
 
-            while ((line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {  // mientras haya una linea para leer, leo
                 Matcher matcher = patron.matcher(line);
                 while (matcher.find()) {
                     for (int i = 1; i <= 8; i++) {
